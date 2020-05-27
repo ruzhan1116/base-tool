97acc34db363b0b369b8324de6727f89a6d00416
ruzhan123 <dev19921116@gmail.com>
2020-05-27 17:29:17 +0800

    update module package name

---
+++ database/src/main/java/com/future/database/CommonModel.kt
@@ -0,0 +1,12 @@
+package com.future.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "common_model")
+data class CommonModel(
+        @PrimaryKey
+        var id: Int,
+        @ColumnInfo(name = "content") var content: String
+)
---
+++ email/src/main/java/com/future/email/MyAuthenticator.kt
@@ -0,0 +1,15 @@
+package com.future.email
+
+import javax.mail.Authenticator
+import javax.mail.PasswordAuthentication
+
+
+class MyAuthenticator(username: String, password: String) : Authenticator() {
+
+    private var userName: String? = username
+    private var password: String? = password
+
+    override fun getPasswordAuthentication(): PasswordAuthentication {
+        return PasswordAuthentication(userName, password)
+    }
+}
